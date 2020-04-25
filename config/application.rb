f93f520d208968aaaa8099bacc2f064c1a6395d7
horiso0921 <horiso0921@gmail.com>
2020-04-25 14:09:40 +0000

    [end:chapter3]fixVagrantfile

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Baukis2
@@ -11,6 +11,19 @@ module Baukis2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = "Tokyo"
+    config.i18n.load_path +=
+      Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_fremework :rspec
+      g.controller_specs false
+      g.view_space false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ config/initializers/blocked_hosts.rb
@@ -0,0 +1,3 @@
+Rails.application.configure do
+    config.hosts = nil
+end
