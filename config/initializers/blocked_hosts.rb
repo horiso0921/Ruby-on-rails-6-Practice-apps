2d2e0e8dda4f28b5c1e9915911b00640fd4b700e
horiso0921 <horiso0921@gmail.com>
2020-05-11 08:56:12 +0000

    [end]5-2-2

--- config/initializers/blocked_hosts.rb
+++ config/initializers/blocked_hosts.rb
@@ -1,3 +1,4 @@
 Rails.application.configure do
-    config.hosts = nil
+    config.hosts << "example.com"
+    config.hosts << "baukis2.example.com"
 end
---
+++ test/controllers/customer/top_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Customer::TopControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
