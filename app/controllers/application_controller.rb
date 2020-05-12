377ce7bf3b9e6ad60e4542a0fe31531c197a3ed8
horiso0921 <horiso0921@gmail.com>
2020-05-12 09:51:04 +0000

    [add]error処理(500,404,403,433)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,34 @@
 class ApplicationController < ActionController::Base
     layout :set_layout
 
+    class Forbidden < ActionController::ActionControllerError; end
+    class IpAddressRejected < ActionController::ActionControllerError; end 
+    
+    rescue_from StandardError, with: :rescue500
+    rescue_from Forbidden, with: :rescue403
+    rescue_from IpAddressRejected, with: :rescue403
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+
     private def set_layout
         if params[:controller].match(%r{\A(staff|admin|customer)/})
             Regexp.last_match[1]
         else
             "customer"
         end
+    end
+
+    private def rescue403(e)
+        @exception = e
+        render "errors/forbidden", status: 403
+    end
+
+    private def rescue404(e)
+        @exception = e
+        render "errors/not_found", status: 404
+    end
+    
+
+    private def rescue500(e)
+        render "errors/internal_server_error", status:500    
     end
 end
---
+++ config/initializers/exceptions_app.rb
@@ -0,0 +1,13 @@
+Rails.application.configure do
+    config .exceptions_app = ->(env) do
+        request = ActionDispatch::Request.new(env)
+
+        action = 
+            case request.path_info
+            when "/404"; :not_found
+            when "/422"; :unprocessavle_entity
+            else; :internal_server_error
+            end
+        ErrorsController.action(action).call(env)
+    end
+end
